1fe6b2802c95e41977484e0b4c0730cfceddc2d8
zhouxingxue <crane.resigned@qq.com>
2024-08-20 19:03:08 +0800

    后端使用说明

--- src/pages/UserEdit.vue
+++ src/pages/UserEdit.vue
@@ -29,7 +29,7 @@ const onSubmit = () => {
     </van-cell-group>
     <div style="margin: 16px;">
       <van-button round block type="primary" native-type="submit">
-        提交
+        确认
       </van-button>
     </div>
   </van-form>
