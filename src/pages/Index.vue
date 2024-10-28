3a599e2bdd221a01495b310645f1a9d7078e539b
Crane Resigned <crane.resigned@qq.com>
2024-10-28 23:07:04 +0800

    心动搜索，骨架屏

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,7 @@ import {onMounted, ref} from "vue";
 import myAxios from "../plugins/axios"
 import {UserType} from "../model/user.js";
 import {getCurrentUser} from "../services/user.ts";
+import {showFailToast, showSuccessToast} from "vant";
 
 const router = useRouter();
 
@@ -31,6 +32,24 @@ onMounted(async () => {
   }
 })
 
+const loadingMatch = ref(false)
+const doMatchUsers = async () => {
+  loadingMatch.value = true
+  const num = 10;
+  const res = await myAxios.get("/user/usersMatch", {
+    params: {
+      num,
+    }
+  })
+  if (res.code === 20000) {
+    showSuccessToast("匹配成功")
+    users.value = res.data;
+  } else {
+    showFailToast(res.description)
+  }
+  loadingMatch.value = false
+}
+
 </script>
 
 <template>
@@ -46,16 +65,23 @@ onMounted(async () => {
         text="寻找与你相似的人。"
     />
 
-    <van-card
-        v-for="user in users"
-        :desc="user.introduction"
-        :title="user.nickname"
-        :thumb="user.avatarUrl"
-    >
-      <template #tags>
-        <van-tag plain mark type="primary" style="margin-right: 0.5em" v-for="tag in user.tags">{{ tag }}</van-tag>
-      </template>
-    </van-card>
+    <van-button type="primary" round hairline @click="doMatchUsers">寻找与你相似的用户</van-button>
+    <div>超强算法，助你寻找心动之人，但耗时较长</div>
+
+    <van-skeleton title avatar :row="3" :loading="loadingMatch" v-for="user in users">
+      <van-card
+          :desc="user.introduction"
+          :title="user.nickname"
+          :thumb="user.avatarUrl"
+      >
+        <template #tags>
+          <van-tag plain mark type="primary" style="margin-right: 0.5em" v-for="tag in user.tags">{{ tag }}</van-tag>
+        </template>
+      </van-card>
+
+    </van-skeleton>
+
+
   </template>
 
 </template>
